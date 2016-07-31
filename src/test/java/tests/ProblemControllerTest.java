baf4203811ba5eea47cb960a43b41b8d8a1ca881
thiagoasd <thiago_teixeira4@yahoo.com.br>
2016-07-30 22:38:42 -0300

    Commenting and explaining tests on ProblemaTests

--- src/test/java/tests/ProblemControllerTest.java
+++ src/test/java/tests/ProblemControllerTest.java
@@ -9,7 +9,7 @@ import models.Problema;
 public class ProblemControllerTest {
 
 	@Test
-	public void test() {
+	public void ProblemaTest() {
 
 		// Right now we only return a Json with one Problema
 		get("/Problema").then().body("id", hasItem(0)).assertThat().statusCode(200);
@@ -19,12 +19,15 @@ public class ProblemControllerTest {
 		get("/Problema").then().body("descricao", hasItem("descricao"));
 		get("/Problema").then().body("id", hasItem(0));
 
+		// Test Post and checks if the correct Json is been saved
 		Problema prob = new Problema("name", "cod", "dica", "desc");
 		given().contentType(ContentType.JSON).body(prob).when().post("/Problema").then().assertThat().statusCode(200)
 				.body("nome", is("name"));
 
+		//Tests GET with Path Variable
 		get("/Problema/0").then().body("id", equalTo(0)).assertThat().statusCode(is(200));
 
+		//Tests PUT and checking if it really changed the JSON
 		prob.setNome("novoNome");
 		given().contentType(ContentType.JSON).body(prob).when().put("/Problema/0").then().assertThat()
 				.statusCode(is(200)).body("nome", is("novoNome"));
@@ -33,9 +36,11 @@ public class ProblemControllerTest {
 		given().contentType(ContentType.JSON).body(prob).when().put("/Problema/0").then().assertThat()
 				.statusCode(is(200)).body("codigo", is("111"));
 
+		//Tests DELETE
 		delete("/Problema/0").then().assertThat().statusCode(is(200));
-		get("/Problema/1").then().assertThat().body("id", is(1)).statusCode(is(200));
-
+		
 	}
+	
+	
 
 }
